564723d3ff41e7a70cb439b6f282dc99e5d887e6
keepish <vdksemaluv@icloud.com>
2025-09-18 15:30:57 +0300

    Scaffold done.
    + Models
    + DB Context

---
+++ DataLayer/Models/Promocode.cs
@@ -0,0 +1,14 @@
+﻿namespace DataLayer.Models;
+
+public partial class Promocode
+{
+    public int Id { get; set; }
+
+    public string Code { get; set; } = null!;
+
+    public DateTime? StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+
+    public bool IsActive { get; set; }
+}
