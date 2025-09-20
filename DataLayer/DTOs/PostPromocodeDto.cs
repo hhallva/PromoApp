4b5904d6423e2ddddd04e7f5d983c894a349e8db
Илья <golyshevia@gmail.com>
2025-09-20 19:48:19 +0300

    Добавлена модель для создания промокода через  API

---
+++ DataLayer/DTOs/PostPromocodeDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DataLayer.DTOs
+{
+    public class PostPromocodeDto
+    {
+        public string Code { get; set; } = null!;
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+    }
+}
